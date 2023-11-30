95f1d37f7dfec878ca93e3368d857824410b2fea
Poorvee-Git1999 <pbamhroliya@gmail.com>
2023-11-30 15:36:45 +0530

    made changes in code

--- force-app/main/default/lwc/experiencePageComponent/experiencePageComponent.js
+++ force-app/main/default/lwc/experiencePageComponent/experiencePageComponent.js
@@ -1,7 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement ,wire} from 'lwc';
 import myResource from '@salesforce/resourceUrl/ExperiencePage';
+import fetchExperienceData from '@salesforce/apex/ExperienceData.fetch';
 
 export default class ExperiencePageComponent extends LightningElement {
     companyLogoUrl = myResource;
+    experienceData;
+
+    @wire(fetchExperienceData)
+    wiredExperienceData({ error, data }) {
+        if (data) {
+            this.experienceData = data;
+        } else if (error) {
+            console.error('Error fetching experience data', error);
+        }
+    }
 
 }
