8304b4aa4176784c711d6b7aa447ebe6cc8ffc0c
Poorvee-Git1999 <pbamhroliya@gmail.com>
2023-12-14 11:16:11 +0530

    new code

--- force-app/main/default/lwc/experiencePageComponent/experiencePageComponent.js
+++ force-app/main/default/lwc/experiencePageComponent/experiencePageComponent.js
@@ -1,15 +1,23 @@
-import { LightningElement ,wire} from 'lwc';
+import { LightningElement ,wire,track} from 'lwc';
 import myResource from '@salesforce/resourceUrl/ExperiencePage';
 import fetchExperienceData from '@salesforce/apex/ExperienceData.fetch';
 
 export default class ExperiencePageComponent extends LightningElement {
     companyLogoUrl = myResource;
     experienceData;
-
+   @track experienceArray=[];
     @wire(fetchExperienceData)
     wiredExperienceData({ error, data }) {
         if (data) {
             this.experienceData = data;
+            this.experienceData.forEach(experiences => {
+                if (experiences.experience.Is_Featured__c == true) {
+                    console.log('hello');
+                    this.experienceArray.push(experiences);
+                    console.log('his.experienceArray----->',this.experienceArray);
+                    console.log('the balue of is ' ,JSON.parse(JSON.stringify(this.experienceArray)));
+                }
+            });
             console.log('the data is ' ,this.experienceData)
         } else if (error) {
             console.error('Error fetching experience data', error);
