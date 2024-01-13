f52bf8735a155616cd73e67db5d63de0f7ac3a1c
Poorvee-Git1999 <pbamhroliya@gmail.com>
2024-01-13 22:49:33 +0530

    added files

---
+++ force-app/main/default/lwc/challenge6movieDetail/challenge6movieDetail.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Challenge6movieDetail extends LightningElement {}
---
+++ force-app/main/default/lwc/challenge6movieSearch/challenge6movieSearch.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Challenge6movieSearch extends LightningElement {}
